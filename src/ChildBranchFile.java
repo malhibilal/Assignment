905bab95a34dabd1a638a85bd1fcf3a2c543567f
BilalEjaz <malhibilal@gmail.com>
2022-11-09 13:47:58 +0100

    method created in a java class in the firstchild branch

---
+++ src/ChildBranchFile.java
@@ -0,0 +1,8 @@
+public class ChildBranchFile {
+String city = "Stockholm";
+String country = "Sweden";
+
+public void info(){
+    System.out.println("the name of the country is "+country+ " and the name of the city is "+city);
+}
+}
