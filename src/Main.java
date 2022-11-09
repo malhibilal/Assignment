d88b4c75c846294ea7bc02763f1de2153244adef
BilalEjaz <malhibilal@gmail.com>
2022-11-09 13:29:25 +0100

    new method added from the childbranch class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Main {
     public static void main(String[] args) {
 
 
         System.out.println("this is my first git assignment");
         Assignment asg = new Assignment();
+        ChildBranchFile cbf = new ChildBranchFile();
+        System.out.println("-------------");
         asg.printinfo();
+        System.out.println("-------------");
+        cbf.info();
     }
 }
