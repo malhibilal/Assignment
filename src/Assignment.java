8bb9b12cfb57d3a769a111217f2b4615069da54a
BilalEjaz <malhibilal@gmail.com>
2022-11-09 13:19:09 +0100

    new assignmnet file created with a method

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+    String name = "john";
+    int age = 32;
+    public void printinfo(){
+        System.out.println( "name of the person is "+ name+ " and the age is "+ age );
+    }
+
 }
