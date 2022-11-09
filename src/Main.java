cb2bffcace2f2b3632975c4743a8145086b92eae
BilalEjaz <malhibilal@gmail.com>
2022-11-09 13:16:52 +0100

    main class committed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("this is my first git assignment");
+        Assignment asg = new Assignment();
+        asg.printinfo();
+    }
+}
